e07a6364640128450a607db38ae4f79480a0eeac
catmaldo03 <138506870+catmaldo03@users.noreply.github.com>
2023-07-22 19:27:49 -0300

    Acercamiento del mapa juego005.c
    En esta actualizacion del codigo, lo que se quizo implementar es el acercamiento del mapa

--- juego005.c
+++ juego005.c
@@ -2,13 +2,11 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/file.h>
 #include <allegro5/allegro_font.h>
-#include<stdio.h>
-#define ANCHO 30
-#define LARGO 41
-#define PIXEL 20
-#define MOV 20
-#define MAX 6
-#define cantidad 2
+#define ANCHO 15
+#define LARGO 20
+#define PIXEL 40
+#define MOV 40
+#define cantidad 10
 #define KEY_SEEN     1
 #define KEY_RELEASED 2
 struct JUG{
@@ -23,41 +21,35 @@ struct enemigo{
     int indicacion;
     int vida;
     int activo;
-    int flech_x[MAX];
     //poner aqui la variable indicacion y contador
 
 };
-
-void  cargamapa(char map[ANCHO][LARGO], ALLEGRO_FILE* file);
-void posjugador(char map[ANCHO][LARGO],struct JUG* jugador);
 void posenemigo(struct enemigo en [cantidad],char map[ANCHO][LARGO]);
+void  cargamapa(char map[ANCHO][LARGO], ALLEGRO_FILE* file);
+void posjugador(char map[ANCHO][LARGO],struct JUG* jugador, int ubicacion);
 int main(){
-   typedef struct enemigo ENE;
-   typedef struct JUG PERSONA;
+    typedef struct JUG PERSONA;
    PERSONA jugador;
+   typedef struct enemigo ENE;
    ENE en [cantidad];
-   int x, y, posxj, posyj,enx,eny,cont,efx,i,velocidad=0,velocidad_2=0;
-   int flech_pos, flech_y;
+   int x, y, posxj, posyj,enx,eny,cont,efx,i,velocidad=0,velocidad_2=0,mapa,ubicacion=0,a;
    char map[ANCHO][LARGO];
-   //variable contador que regule la velocidad del enemigo
-//----------Inicializaciones---------------------------------
-   al_init();
+    al_init();
    al_install_keyboard();
    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
    ALLEGRO_DISPLAY* disp = al_create_display(800, 600); 
    al_init_image_addon();
-   ALLEGRO_FILE* file = al_fopen("mapa1.txt", "r");
+   ALLEGRO_FILE* file = al_fopen("file1.txt", "r");
+   mapa=1;
    al_start_timer(timer);
    ALLEGRO_FONT* font = al_create_builtin_font();
-   // ----Inicializaciones DE bitmap------
    ALLEGRO_BITMAP* lim = al_load_bitmap("pinch.png");
-   ALLEGRO_BITMAP* muro = al_load_bitmap("roca1.png");
-   ALLEGRO_BITMAP* jug = al_load_bitmap("jug.png");
+   ALLEGRO_BITMAP* muro = al_load_bitmap("muro2.png");
+   ALLEGRO_BITMAP* jug = al_load_bitmap("X2.png");
    ALLEGRO_BITMAP* fal = al_load_bitmap("falso.png");
    ALLEGRO_BITMAP* ene = al_load_bitmap("ene.png");
    ALLEGRO_BITMAP* flecha = al_load_bitmap("flecha.png");
-   //---eventos-------
    al_register_event_source(queue, al_get_keyboard_event_source());
    al_register_event_source(queue, al_get_display_event_source(disp));
    al_register_event_source(queue, al_get_timer_event_source(timer));
@@ -68,70 +60,32 @@ int main(){
    al_start_timer(timer);
 
    cargamapa(map,file);
- posjugador(map,&jugador); 
+ posjugador(map,&jugador,ubicacion); 
  posxj =jugador.bitmap_x;
  posyj=jugador.bitmap_y; 
-//posiciona al enemigo
- posenemigo(en,map);
-
-
-   unsigned char key[ALLEGRO_KEY_MAX];
+unsigned char key[ALLEGRO_KEY_MAX];
    memset(key, 0, sizeof(key));
    jugador.vida = 3;
-   en [1].indicacion=0;
-   cont=0;
-   en [1].vida=3;
-   en [1].activo=1;
-   for(i=0;i<MAX;i++)
+   while(1)
    {
-   en [0].flech_x[i]= en[0].e_x;
-   en [0].flech_x[i]= en [0].flech_x[i]-20;
-   }
-   i=0;
-   while (1){
-      al_wait_for_event(queue, &event);
+    al_wait_for_event(queue, &event);
       switch(event.type)
       {
        case ALLEGRO_EVENT_TIMER:
-       //enemigo en[1]-----------------------------------------------------------------------------------
-       printf("%d",velocidad);
-       if(velocidad==0)
+       if(mapa==2)
+
+       {
+         velocidad=(velocidad+1)%4;
+        if(velocidad==0)
         {
-      
-        if(en [1].indicacion==0&&cont<2&&en [1].activo==1)
-        {
+          if(enx<x)
+          {
             en [1].e_x+=MOV;
+
+          }
         }
-        if(map[eny][enx+2] == 'm'&& en [1].indicacion==0&&en [1].activo==1)
-        {
-            en [1].e_x-=MOV;
-            en [1].indicacion =1;
-            cont++;
-        }
-            
-        if(en [1].indicacion==1&&en [1].activo==1)
-        {
-             en [1].e_y+=MOV;
-        }
-        if(map[eny+2][enx] == 'm'&& en [1].indicacion==1&&en [1].activo==1)
-        {
-            en [1].e_y-=MOV;
-            en [1].indicacion =0;
-        }
-        if(cont==2 && en [1].indicacion==0&&en [1].activo==1)
-        {
-         en [1].e_x-=MOV;
-        }
-        if(cont==2 && map[eny][enx-2]== 'm'&&en [1].indicacion==0&&en [1].activo==1)
-        {
-            en [1].e_x+=MOV;
-            cont--;
-        }
-        
-        }
-        velocidad=(velocidad+1)%4;
-        //jugador-------------------------------------------------------------------------------------------------------
-        if(key[ALLEGRO_KEY_UP]){
+       }
+       /*sif(key[ALLEGRO_KEY_UP]){
             jugador.bitmap_y-=MOV;
             if (map[y-1][x] == 'm')
             {
@@ -166,34 +120,52 @@ int main(){
             {
               jugador.bitmap_x-=MOV;
             }
-        }
-        //en [0]----------------------------------------------------------------------------------
-        velocidad_2=(velocidad_2+1)%80;
-       if(velocidad==0)
-        {
-            en[0].flech_x[i]-=MOV;
-            flech_pos=en[0].flech_x[i]/20;
-            if (map[en[0].e_y][flech_pos-1] == 'm')
-            {
-              en[0].flech_x[i]+=MOV;
-            }
-            
-        } 
-        if(velocidad_2==0)
-            {
-                i++;
-            }
-        printf("%d", velocidad_2);
-        if(key[ALLEGRO_KEY_ESCAPE])
-            done = true;
-
-        for(int i = 0; i < ALLEGRO_KEY_MAX; i++)
-            key[i] &= KEY_SEEN;
-       
+        }*/
         redraw = true;
         break;
 
-       case ALLEGRO_EVENT_KEY_DOWN:
+        case ALLEGRO_EVENT_KEY_DOWN:
+       //enemigo 1--------
+        if(event.keyboard.keycode == ALLEGRO_KEY_UP)
+        {
+            jugador.bitmap_y-=MOV;
+            if (map[y-1][x] == 'm')
+            {
+              jugador.bitmap_y+=MOV;
+            }
+        }
+        if(event.keyboard.keycode == ALLEGRO_KEY_DOWN)
+        {
+            jugador.bitmap_y+=MOV;
+            if (map[y+1][x] == 'm')
+            {
+              jugador.bitmap_y-=MOV;
+            }
+        }
+        if(event.keyboard.keycode == ALLEGRO_KEY_LEFT)
+        {
+            jugador.bitmap_x-=MOV;
+            if (map[y][x-1] == 'm')
+            {
+              jugador.bitmap_x+=MOV;
+            }
+            if(map[y][x-1] == 't')
+            {
+              jugador.bitmap_x+=MOV;
+            } 
+        }
+        if(event.keyboard.keycode == ALLEGRO_KEY_RIGHT)
+        {
+            jugador.bitmap_x+=MOV;
+            if (map[y][x+1] == 'm')
+            {
+              jugador.bitmap_x-=MOV;
+            }
+            if(map[y][x+1] == 't')
+            {
+              jugador.bitmap_x-=MOV;
+            } 
+        }
         if (event.keyboard.keycode== ALLEGRO_KEY_Z)
         {
           if(map[y][x+1] == 't')
@@ -206,22 +178,8 @@ int main(){
             {
               map[y][x-1] ='-';
             }
-          if(jugador.bitmap_x+MOV==en [1].e_x)
-          {
-            en [1].vida--;
-          }
-          if(en [1].vida==0)
-          {
-            for(y = 0; y < ANCHO; y++){
-              for (x = 0; x < LARGO; x++) 
-            {
-               if(map[y][x] == 'E') 
-                 map[y][x] ='-';
-            }
-            }
-          }
-        }
-        key[event.keyboard.keycode] = KEY_SEEN | KEY_RELEASED;
+       }
+   key[event.keyboard.keycode] = KEY_SEEN | KEY_RELEASED;
         break;
        case ALLEGRO_EVENT_KEY_UP:
         key[event.keyboard.keycode] &= KEY_RELEASED;
@@ -235,10 +193,11 @@ int main(){
          break;
       if (redraw && al_is_event_queue_empty(queue)) {   
          al_clear_to_color(al_map_rgb(255, 255, 255));
+         for(i=0;i<cantidad && en[i].activo==1;i++)
+         {
+         al_draw_bitmap(ene, en [i].e_x, en [i].e_y,0);
+         }
          al_draw_bitmap(jug, jugador.bitmap_x, jugador.bitmap_y, 0);//el jugador
-         al_draw_bitmap(ene, en [0].e_x, en [0].e_y,0);
-         al_draw_bitmap(ene, en [1].e_x, en [1].e_y,0);
-          al_draw_bitmap(flecha,en[0].flech_x[i], en [0].e_y,0);
          for (y = 0; y < ANCHO; y++) 
          {
             for (x = 0; x < LARGO; x++) 
@@ -258,47 +217,272 @@ int main(){
                }
             }
          }
-         
-         
-//-------calculos del mapa--------------
-   x=jugador.bitmap_x/PIXEL;
+    x=jugador.bitmap_x/PIXEL;
    y=jugador.bitmap_y/PIXEL;
    enx=en [1].e_x/PIXEL;
    eny= en [1].e_y/PIXEL;
-   al_draw_textf(font, al_map_rgb(0, 0, 0), 0, 0, 0, "bitx: %d, bity: %d, x: %d, y: %d,vida %d, %d,%d, eny:%d,enx%d,env:%d, flecha: %d,i:%d",jugador.bitmap_x, jugador.bitmap_y,x,y,jugador.vida,en [1].e_x,en [1].e_y,eny,enx,en [1].vida, flech_pos,i);
-//------otras cosas--------
+   al_draw_textf(font, al_map_rgb(0, 0, 0), 0, 0, 0, "bitx: %d, bity: %d, x: %d, y: %d,mapa:%d,ubicacion:%d",jugador.bitmap_x, jugador.bitmap_y,x,y,mapa,ubicacion);
+   if(map[y][x] == 'S'&&mapa==1)
+   {
+    mapa=2;
+    ALLEGRO_FILE* file = al_fopen("file2.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
    
-   if(map[y][x] == 'p')
-   {
-      jugador.vida-= 1;
-      jugador.bitmap_x = posxj;
-      jugador.bitmap_y = posyj;
+   ubicacion=0;
    }
-   if((x == enx)&&(y == eny))
+   if(map[y][x] == 'S'&&mapa==2)
    {
-      jugador.vida-= 1;
-      jugador.bitmap_x = posxj;
-      jugador.bitmap_y = posyj;
-   } 
-   if(jugador.vida==0)
-   {
-      done = true;
-      break;
-   }
-   if(en [1].vida==0)
-   {
-     en [1].activo=0;
-     en [1].e_x=0;
-     en [1].e_y=0;
-   }
-   if(i==(MAX-1))
-   {
-    i=0;
-    en [0].flech_x[i]= en[0].e_x;
-   en [0].flech_x[i]= en [0].flech_x[i]-20;
-   }   
+    mapa=3;
+    ALLEGRO_FILE* file = al_fopen("file3.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=1;
 
-      al_flip_display();
+   }
+   if(map[y][x] == 'e'&&mapa==3)
+   {
+    mapa=6;
+    ALLEGRO_FILE* file = al_fopen("file6.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=1;
+
+   }
+   if(map[y][x] == 'N'&&mapa==6)
+   {
+    mapa=5;
+    ALLEGRO_FILE* file = al_fopen("file5.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=0;
+
+   }
+   if(map[y][x] == 'e'&&mapa==6)
+   {
+    mapa=9;
+    ALLEGRO_FILE* file = al_fopen("file9.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=1;
+
+   }
+   if((map[y][x] == 'e'&&mapa==5)||(map[y][x] == 'N'&&mapa==9))
+   {
+    mapa=8;
+    ALLEGRO_FILE* file = al_fopen("file8.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=0;
+
+   }
+   if(map[y][x] == 'e'&&mapa==8)
+   {
+    mapa=11;
+    ALLEGRO_FILE* file = al_fopen("file11.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=1;
+
+   }
+   if(map[y][x] == 'e'&&mapa==9)
+   {
+    mapa=12;
+    ALLEGRO_FILE* file = al_fopen("file12.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=1;
+
+   }
+   if(map[y][x] == '1'&&mapa==1)
+   {
+    
+    ALLEGRO_FILE* file = al_fopen("file4.txt", "r");
+    cargamapa(map,file);
+    posjugador(map,&jugador,ubicacion); 
+    posxj =jugador.bitmap_x;
+    posyj=jugador.bitmap_y;
+    ubicacion=3;
+    mapa=4;
+   }
+   if(map[jugador.bitmap_y/PIXEL][jugador.bitmap_x/PIXEL]=='e'&&mapa==4)
+    {
+      ALLEGRO_FILE* file = al_fopen("file7.txt", "r");
+      cargamapa(map,file);
+      posjugador(map,&jugador,ubicacion); 
+      posxj =jugador.bitmap_x;
+      posyj=jugador.bitmap_y;
+      ubicacion=1;
+      mapa=7;
+    }
+   if(map[jugador.bitmap_y/PIXEL][jugador.bitmap_x/PIXEL]=='1'&&mapa==7){
+    ALLEGRO_FILE* file = al_fopen("file10.txt", "r");
+      cargamapa(map,file);
+      posjugador(map,&jugador,ubicacion); 
+      posxj =jugador.bitmap_x;
+      posyj=jugador.bitmap_y;
+      ubicacion=0;
+      mapa=10;
+   }
+   if(map[y][x] == 'N'&&mapa==2)
+   {
+    mapa=1;
+    ALLEGRO_FILE* file = al_fopen("file1.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   
+   ubicacion=0;
+   }
+   if(map[y][x] == 'o'&&mapa==4)
+   {
+    mapa=1;
+    ALLEGRO_FILE* file = al_fopen("file1.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   
+   ubicacion=0;
+   }
+   if(map[y][x] == 'o'&&mapa==7)
+   {
+    mapa=4;
+    ALLEGRO_FILE* file = al_fopen("file4.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=3;
+   }
+    if(map[y][x] == 'o'&&mapa==10)
+   {
+    mapa=7;
+    ALLEGRO_FILE* file = al_fopen("file7.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=1;
+   }
+   if(map[y][x] == 'N'&&mapa==3)
+   {
+    mapa=2;
+    ALLEGRO_FILE* file = al_fopen("file2.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=0;
+   }
+   if(map[y][x] == 'o'&&mapa==6)
+   {
+    mapa=3;
+    ALLEGRO_FILE* file = al_fopen("file3.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=1;
+   }
+   if(map[y][x] == 'S'&&mapa==5)
+   {
+    mapa=6;
+    ALLEGRO_FILE* file = al_fopen("file6.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=1;
+   }
+   if(map[y][x] == 'o'&&mapa==8)
+   {
+    mapa=5;
+    ALLEGRO_FILE* file = al_fopen("file5.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=0;
+   }
+   if(map[y][x] == 'S'&&mapa==8)
+   {
+    mapa=9;
+    ALLEGRO_FILE* file = al_fopen("file9.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=1;
+   }
+    if(map[y][x] == 'o'&&mapa==9)
+   {
+    mapa=6;
+    ubicacion=3;
+    ALLEGRO_FILE* file = al_fopen("file6.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=1;
+   }
+    if(map[y][x] == 'o'&&mapa==12)
+   {
+    mapa=9;
+    ubicacion=3;
+    ALLEGRO_FILE* file = al_fopen("file9.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=1;
+   }
+    if(map[y][x] == 'o'&&mapa==11)
+   {
+    mapa=8;
+    ubicacion=3;
+    ALLEGRO_FILE* file = al_fopen("file8.txt", "r");
+    cargamapa(map,file);
+ posjugador(map,&jugador,ubicacion); 
+ posxj =jugador.bitmap_x;
+ posyj=jugador.bitmap_y;
+   ubicacion=1;
+   }
+  if(mapa==2)
+  {
+    posenemigo(en,map);
+    for(i=0;i<cantidad;i++)
+    {
+      en[i].activo=1;
+      en[i].vida=3;
+    }
+  }
+  else{
+    for(i=0;i<cantidad;i++)
+    {
+      en[i].activo=0;
+      en[i].vida=0;
+    }
+  }
+
+   al_flip_display();
       redraw = false;
       }
    }
@@ -312,20 +496,25 @@ int main(){
 }
 void  cargamapa(char map[ANCHO][LARGO], ALLEGRO_FILE* file){
    // Leer el mapa del archivo
-   int y,x;
+   int y,x,aux;
    for (y = 0; y < ANCHO; y++) {
       for (x = 0; x < LARGO; x++) {
          map[y][x] = al_fgetc(file);
+         if(x==(LARGO-1))
+         {
+            aux=al_fgetc(file);
+         }
       }
    }
    al_fclose(file);
 }
-void posjugador(char map[ANCHO][LARGO],struct JUG* jugador){
+void posjugador(char map[ANCHO][LARGO],struct JUG* jugador,int ubicacion){
    int y,x;
    for (y = 0; y < ANCHO; y++) 
    {
      for (x = 0; x < LARGO; x++) 
      {
+        if(ubicacion==0){
         if (map[y][x] == 'J') 
         {
            jugador->bitmap_x=x;
@@ -333,6 +522,26 @@ void posjugador(char map[ANCHO][LARGO],struct JUG* jugador){
            jugador->bitmap_x=jugador->bitmap_x*PIXEL;
            jugador->bitmap_y=jugador->bitmap_y*PIXEL;
          }
+        }
+        if(ubicacion==1)
+        {
+          if (map[y][x] == 'j') 
+        {
+           jugador->bitmap_x=x;
+           jugador->bitmap_y=y;
+           jugador->bitmap_x=jugador->bitmap_x*PIXEL;
+           jugador->bitmap_y=jugador->bitmap_y*PIXEL;
+         }  
+        }
+        if(ubicacion==3){
+            if (map[y][x] == 'P') 
+        {
+           jugador->bitmap_x=x;
+           jugador->bitmap_y=y;
+           jugador->bitmap_x=jugador->bitmap_x*PIXEL;
+           jugador->bitmap_y=jugador->bitmap_y*PIXEL;
+         } 
+        }
 
       }
    }
